0edb60b69abaf695a28396c6ddb2aff009a3c629
tecskp1 <tec@undefinedgames.org>
2018-04-17 08:48:44 +0200

    First commit

---
+++ app/src/main/java/com/example/tec_programmoer/simonsaysapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.tec_programmoer.simonsaysapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
